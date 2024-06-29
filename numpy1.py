d6b757840bf14d06c79af6f5fc4eb38abcf1222f
PapiyaMaity <165825022+PapiyaMaity@users.noreply.github.com>
2024-06-29 20:17:01 +0530

    Add files via upload

---
+++ numpy1.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+my_list=[1,5,10,15,20]
+
+
+# In[3]:
+
+
+type(my_list)
+
+
+# In[6]:
+
+
+arr=np.array(my_list)
+
+
+# In[7]:
+
+
+arr
+
+
+# In[8]:
+
+
+type(arr)
+
+
+# In[9]:
+
+
+my_matrix=[[1,2,3],[4,5,6],[7,8,9]]
+
+
+# In[10]:
+
+
+np.array(my_matrix)
+
+
+# In[4]:
+
+
+import numpy as np
+
+
+# In[11]:
+
+
+myarr=np.linspace(0,10,101)
+
+
+# In[12]:
+
+
+myarr
+
+
+# In[ ]:
+
+
+
+
